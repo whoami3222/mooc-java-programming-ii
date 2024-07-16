803637b116fd4e428e80723118de985049e29c26
David <dawid.szcz@gmail.com>
2024-07-16 21:11:25 +0300

    part10_05 and _06

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -7,5 +7,20 @@ public class LimitedNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (true) {
+            int input = scanner.nextInt();
+            if(input < 0){
+                break;
+            }
+
+            list.add(input);
+            
+        }
+
+        list.stream()
+        .filter(s -> s >= 1 && s <= 5)
+        .forEach(s -> System.out.println(s));
     }
 }
